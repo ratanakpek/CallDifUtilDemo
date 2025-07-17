8cc9b271d58deb06e549bd6f672de062ab58e48c
ratanakpek <ratanakpek088@gmail.com>
2025-07-17 07:04:31 +0700

    Create recyclerview

--- app/src/main/java/ratanak/pek/calldifutildemo/MainActivity.kt
+++ app/src/main/java/ratanak/pek/calldifutildemo/MainActivity.kt
@@ -1,11 +1,35 @@
 package ratanak.pek.calldifutildemo
 
-import androidx.appcompat.app.AppCompatActivity
+
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // data to populate the RecyclerView with
+        // data to populate the RecyclerView with
+        val animalNames = ArrayList<String>()
+        animalNames.add("Horse")
+        animalNames.add("Cow")
+        animalNames.add("Camel")
+        animalNames.add("Sheep")
+        animalNames.add("Goat")
+
+        // set up the RecyclerView
+
+        // set up the RecyclerView
+        val recyclerView = findViewById<RecyclerView>(R.id.rvAnimals)
+        recyclerView.layoutManager = LinearLayoutManager(this)
+        val adapter = MyRecyclerViewAdapter(this, animalNames)
+       // adapter.setClickListener(this)
+        recyclerView.adapter = adapter
     }
+
+
 }
